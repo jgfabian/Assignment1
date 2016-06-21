8caa8b71c2475691e9abc956be280362dbf0d566
John Fabian <John Fabian>
2016-06-20 17:18:48 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,202 @@
+//STEP 1
+
+// var someMonth;
+// function theMonth;
+// CURRENTMONTH;
+// VAR summerMonth;
+// myLibraryAFunction;
+
+// STEP 2
+// 45.24 - numeric literal expression
+// "John" - string literal
+// true
+// null
+
+// STEP 3
+
+// var answer = 5 + 6 + 7;
+//console.log(answer);
+
+// var result = 2 + (7 * 23);
+// console.log(result);
+
+// Step 4
+// var firstName;
+// var strFirstName;
+// var lastName;
+// var strLastName;
+// var address;
+// var strAddress;
+// var city;
+// var strCity;
+// var zipCode;
+// var siZipCode;
+// var yourAge;
+// var siYourAge;
+// var referralSource;
+// var strReferralSource;
+// var mayWeContactYou;
+// var bMayWeContactYou;
+
+// Step 5
+// var firstName;
+// firstName = "John";
+// var firstName = "John";
+// var firstName = "John", lastName = "Fabian";
+
+// var lastName;
+// lastName = "Fabian";
+// var firstName = "John", lastName = "Fabian";
+
+// var address;
+// address = "123 Main Street";
+// var address = "123 Main Street";
+// var address = "123 Main Street", city = "Anytown";
+
+// var city;
+// city = "Anytown";
+// var city = "Anytown";
+// var city = "Anytown", zipCode = 12345;
+
+// var zipCode;
+// zipCode = 12345;
+// var zipCode = 12345;
+// var zipCode = 12345, city = "Anytown";
+
+// var yourAge;
+// yourAge = 45;
+// var yourAge = 45;
+// var yourAge = 45, referralSource = "Wall Street";
+
+// var referralSource;
+// referralSource = "Bill";
+// var referralSource = "Bill";
+// var referralSource = "Bill", yourAge = 45;
+
+// var mayWeContactYou;
+// ayWeContactYou = false;
+// var mayWeContactYou = false;
+// var referralSource = "Bill", mayWeContactYou = false;
+
+// Step 6
+// var age, employd;
+// age = 45;
+// employd = "20";
+// var result = age + employd;
+// console.log(result);  //4520
+
+// var bool, someString;
+// bool = true;
+// someString = "A String";
+// console.log(bool + someString); //trueA String
+
+// var bool, smallNumber;
+// bool = true;
+// smallNumber = 25;
+// console.log(bool + smallNumber); //26
+
+// Step 7
+// console.log(city);
+// var city = "San Diego"; //undefined
+
+// Step 8 
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'; // add /
+// console.log(someString);
+
+// Step 9
+//var notDeclared;
+//console.log(notDeclared); //undefined
+
+// var z = null;
+// console.log(z); // null
+
+// Step 10
+// console.log(typeof "Bill"); 
+// console.log(typeof 51); 
+// console.log(typeof false); 
+// console.log(typeof [1, 2, 3, 4]); 
+// console.log(typeof chair); 
+
+// Step 11
+// var name = "Hello Zak Ruvalcaba,";
+// var welcome = "welcome to the JavaScript class!";
+// var statement = name + " " + welcome;
+// alert(statement);
+
+// Step 12
+// var name = "John Fabian,";
+// var welcome = "welcome to the JavaScript class!";
+// var statement = "Hello" + " " + name + " " + welcome;
+// alert(statement);
+
+// Step 13
+// var name = "John Fabian,";
+// var welcome = "welcome to ";
+// var course = "JavaScript ! ";
+// var statement = "Hello" + " " + name + " " + welcome + course;
+// alert(statement);
+
+// Step 14
+// var name = "John Fabian,";
+// var welcome = "\nWelcome to";
+// var course = "JavaScript ! ";
+// var statement = "Hello" + " " + name + welcome + " " + course;
+// alert(statement);
+
+// Step 15
+// var welcome = "\nWelcome to";
+// var course = "JavaScript ! ";
+// var person = prompt("Please enter your name");
+// var statement = "Hello" + " " + person + welcome + " " + course;
+// alert(statement);
+
+// Step 16
+// var person = prompt("Please enter your name");
+// var course = prompt("Please enter the course you are taking");
+// var welcome = "\nWelcome to";
+// var statement = "Hello" + " " + person + welcome + " " + course;
+// alert(statement);
+
+// Step 17
+// var x = 10;
+// var y = 20;
+// console.log (x + y);
+
+// Step 18
+// var x = 20;
+// console.log (x + 20);
+
+// Step 19
+// var x = 20;
+// console.log(x * 5);
+
+// Step 20
+// var x = ((20 % 3)) / (1);
+// console.log(x);
+
+// Step 21
+// var x = 8;
+// var y = 9;
+// console.log(x < y);
+
+// Step 22
+// var a = 34;
+// var b = 45;
+// console.log(a == b);
+
+// Step 23
+// var n = "widget";
+// console.log(typeof n); 
+
+// Step 23
+// var n = "wiget";
+// console.log(typeof n); // returns string
+
+
+// Step 24
+// var n = [1, 2, 3];
+// console.log(n instanceof Object); // returns true
+
+// Step 25
+// var n = [1, 2, 3];
+// console.log(n instanceof String); // returns false
